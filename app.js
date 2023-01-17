80d0bdc7742375dd2c380b1786593196524a8b2b
Filipov <andrijfilipov73@gmail.com>
2023-01-17 10:22:34 +0200

    fixed scroll

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //add to card
 
 let productsCountElement = document.getElementById("products-count");
-// console.log(productsCountElement.innerHTML);
+
 let addToCardBtns = document.querySelectorAll(".btn_add");
 
 for (let i = 0; i < addToCardBtns.length; i++) {
@@ -10,36 +10,11 @@ for (let i = 0; i < addToCardBtns.length; i++) {
   });
 }
 
-// let pointLike = document.querySelectorAll(".likes");
-
-// let likeClick = document.querySelectorAll(".click-like");
-// for (let i = 0; i < likeClick.length; i++) {
-//   likeClick[i].addEventListener("click", function () {
-//     for (let i = 0; i < pointLike.length; i++) {
-//       if (pointLike[i].getAttribute("src") == "img/like.png") {
-//         pointLike[i].setAttribute("src", "img/like_white.png");
-//       } else {
-//         pointLike[i].setAttribute("src", "img/like.png");
-//       }
-//     }
-//   });
-// }
-
-//ht-18 js likes↓↓↓↓↓
-
 let likeBtns = document.querySelectorAll(".click-like");
 
 likeBtns.forEach((btn) =>
   btn.addEventListener("click", function (e) {
-    // if (btn.classList.contains("liked")) {
-    //   btn.classList.remove("liked");
-    // } else {
-    //   btn.classList.add("liked");
-    // }
-    // btn.classList.toggle("liked");
-
     e.target.classList.toggle("liked");
-    // e.target.style.background = "red";
   })
 );
 
@@ -59,12 +34,15 @@ closeBtn.addEventListener("click", function () {
   modalWindow.classList.remove("show");
 });
 
-// закрить когда нажимаешь на фоновий екран
+// close modal when click background
+
 modalWindow.addEventListener("click", function (e) {
   if (e.target === modalWindow) {
     modalWindow.classList.remove("show");
   }
 });
+
+// slider
 
 $(document).ready(function () {
   $(".sliders").slick({
@@ -73,8 +51,10 @@ $(document).ready(function () {
   });
 });
 
-window.addEventListener("scroll", function () {
-  if (pageYOffset > 1200 && pageYOffset < 1300) {
+function showModalByScroll() {
+  if (scrollY >= document.body.scrollHeight / 2) {
     modalWindow.classList.add("show");
+    window.removeEventListener("scroll", showModalByScroll);
   }
-});
+}
+window.addEventListener("scroll", showModalByScroll);
